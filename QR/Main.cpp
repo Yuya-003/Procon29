386bb96d9eecfc4dffe711b3fccda679b3fd0237
Sha4827 <seahorses527@gmail.com>
2018-07-26 16:24:20 +0900

    QRコードを検出してテキストファイル出力する機能を追加

---
+++ QR/Main.cpp
@@ -0,0 +1,62 @@
+﻿#include <Siv3D.hpp>
+#include <fstream>
+#include <string>
+
+void Main()
+{
+	Window::Resize(1280, 720);
+
+	Webcam webcam(0);
+
+	webcam.setResolution(1280, 720);
+
+	webcam.start();
+
+	Image image;
+
+	DynamicTexture texture;
+
+	QRDecoder qrDecoder;
+
+	std::ofstream outputfile("test.txt");
+
+	std::string str;
+
+	while (System::Update())
+	{
+		if (webcam.hasNewFrame())
+		{
+			ClearPrint();
+
+			webcam.getFrame(image);
+
+			QRContent contents;
+
+			qrDecoder.decode(image, contents);
+
+			contents.quad.overwriteFrame(image, 6, Palette::Red);
+
+			if (contents.text)
+			{
+				Print << contents.text;
+				str = contents.text.narrow();
+			}
+			else {
+				Print << U"No text";
+			}
+
+			if (!str.empty()) 
+				String wStr(str);
+				if (KeyEnter.down()) {
+					outputfile << str;
+					Print << U"Output";
+				}
+			}
+
+			texture.fill(image);
+		}
+
+
+		texture.draw();
+	}
+}
