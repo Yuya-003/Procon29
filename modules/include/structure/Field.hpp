d1827c9a64b5d9c12cdf48a9bc9153bea1acc354
Yuya-003 <yu1ohmi@gmail.com>
2018-10-02 16:51:49 +0900

    プレイヤーの位置の変数team1,team2を追加

--- modules/include/structure/Field.hpp
+++ modules/include/structure/Field.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <vector>
+#include <array>
 #include <structure/Cell.hpp>
+#include <structure/Point.hpp>
 
 class Field {
 public:
 	std::vector<std::vector<Cell>> cells;
+	std::array<Point, 2> team1;
+	std::array<Point, 2> team2;
 
 	Field(size_t h = 0, size_t w = 0);
 	void resize(size_t h, size_t w);
