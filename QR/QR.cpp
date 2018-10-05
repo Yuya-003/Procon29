ed59616a7eec384a0a1c6dd9286cabd3b8ea4ec7
Shade4827 <seahorses527@gmail.com>
2018-10-05 14:51:00 +0900

    QRコードの情報をFieldに変換するstaticな関数を追加

--- QR/QR.cpp
+++ QR/QR.cpp
@@ -1,11 +1,8 @@
 #include "QR.hpp"
-#include <string>
 
 void Main()
 {
 	Window::Resize(1280, 720);
-
-	bool isWebcamMoving = false;
 
 	Webcam webcam(0);
 
@@ -15,14 +12,14 @@ void Main()
 	Image image;
 	DynamicTexture texture;
 	QRDecoder qrDecoder;
+
 	QRInfo qr;
+	String qrText = U"No text.";
+	static bool isCameraStopped = false;
 
 	while (System::Update())
 	{
-		if (KeyEnter.down()) {
-			webcam.stop();
-			WebcamStop(webcam);
-		}
+		ClearPrint();
 
 		if (webcam.hasNewFrame())
 		{
@@ -36,22 +33,19 @@ void Main()
 
 			if (contents.text)
 			{
-				Print << contents.text;
+				qrText = contents.text;
 				qr.SetString(contents.text.narrow());
 			}
-			else {
-				Print << U"No text";
-			}
-
-			std::string qrContent = qr.GetString();
-			Print << qrContent;
 		}
+
+		if (KeyEnter.down()) {
+			webcam.stop();
+			isCameraStopped = true;
+		}
+
+		Print << qrText;
 
 		texture.fill(image);
 		texture.draw();
 	}
-}
-
-void WebcamStop(Webcam webcam) {
-	
 }
