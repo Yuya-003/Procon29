d3a279f42916c039ca2ca1f835b2323dba3a7cff
Yuya-003 <yu1ohmi@gmail.com>
2018-10-18 00:01:00 +0900

    現在のところまでコミット (注：ビルドはできる状況にない)

--- solver/include/search/BasicSearch.hpp
+++ solver/include/search/BasicSearch.hpp
@@ -6,11 +6,29 @@
 
 #include <vector>
 #include <array>
+#include <cmath>
 
 class BasicSearch{
 public:
 
+    class Node{
+    public:
+
+        enum Status{
+            none, open, closed
+        };
+
+        Status status;
+        Position p;
+        Cell cell;
+        int h_cost; //ヒューリスティクスコスト = 目的地までの距離
+        int cost; //実コスト = 今までかかったコスト
+        int score; //スコア = h_cost + cost - Cellのpoint
+        Node *parent; //親ノード
+    };
+
     int limit_search;
+    int border_score = 10;
 
     BasicSearch();
 
--- solver/src/search/BasicSearch.cpp
+++ solver/src/search/BasicSearch.cpp
@@ -6,4 +6,53 @@ BasicSearch::BasicSearch()
 
 std::array<Behavior, 2> BasicSearch::search(Field field)
 {
+    std::vector<std::vector<Node>> map(field.colSize(), std::vector<Node>(field.rowSize(), Node()));
+
+    std::vector<Node> nodes;
+    for(int k = 0; k <= 1; k++){
+
+        Node goal;
+        goal.cell.point = 1e-6;
+        for(int i = 0; i < field.colSize(); i++){
+            for(int j = 0; j < field.rowSize(); j++){
+                map[i][j].cell = field.cells[i][j];
+                map[i][j].status = Node::Status::none;
+                map[i][j].p = Position(j, i);
+
+                if(map[i][j].cell.status==Cell::none && (map[i][j].cell.point >= goal.cell.point || (map[i][j].cell.point >= border_score && map[i][j].cell.point >= goal.cell.point))){
+                    goal = map[i][j];
+                }
+            }
+        }
+
+        Position player_pos = field.team1[k];
+        map[player_pos.y][player_pos.x].parent = nullptr;
+        map[player_pos.y][player_pos.x].cost = 0;
+        map[player_pos.y][player_pos.x].h_cost = sqrt(pow(goal.p.x-player_pos.x, 2) + pow(goal.p.y-player_pos.y, 2));
+        map[player_pos.y][player_pos.x].score = map[player_pos.y][player_pos.x].h_cost
+                                                + map[player_pos.y][player_pos.x].cost
+                                                - map[player_pos.y][player_pos.x].cell.point;
+        
+        nodes.push_back(map[player_pos.y][player_pos.x]);
+        while(!nodes.empty()){
+            Node node = nodes[0];
+            for(int i = 1; i < nodes.size(); i++){
+                if(node.score < nodes[i].score){
+                    node = nodes[i];
+                }
+            }
+
+            //以下にいろいろ追加
+
+        }
+
+    }
+
+    //とりあえずなんかリターン
+    std::array<Behavior, 2> behavior;
+    behavior[0].action = Behavior::Action::stay;
+    behavior[0].dir = Behavior::Dir::none;
+    behavior[1].action = Behavior::Action::stay;
+    behavior[1].dir = Behavior::Dir::none;
+    return behavior; 
 }
