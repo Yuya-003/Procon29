bf586797823f58c1ae7fb49481084c12d9c93e2b
Yuya-003 <yu1ohmi@gmail.com>
2018-10-18 00:00:10 +0900

    Dirにnoneを追加

--- solver/include/structure/Behavior.hpp
+++ solver/include/structure/Behavior.hpp
@@ -4,10 +4,10 @@ class Behavior{
 public:
     enum Action{
         move, stay, erase
-    };
+    };s
 
     enum Dir{
-        up, down, left, right, upleft, upright, downleft, downright
+        up, down, left, right, upleft, upright, downleft, downright, none
     };
 
     Action action;
