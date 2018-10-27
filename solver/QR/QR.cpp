67d1e69bfe0dc704131315b272c071a25600ca29
Shade4827 <seahorses527@gmail.com>
2018-10-27 11:10:59 +0900

    読み込んだ情報をfield.txtに書き込む機能を追加

--- solver/QR/QR.cpp
+++ solver/QR/QR.cpp
@@ -1,55 +1,82 @@
 #include "QR.hpp"
 
-void Main()
+std::string QRInfo::qrText = "";
+bool QRInfo::isCameraStopped = false;
+
+std::vector<std::string> QRInfo::Split(const std::string & str, char del)
 {
-	Webcam webcam(0);
-	Image image;
-	DynamicTexture texture;
-	QRDecoder qrDecoder;
-	QRInfo qr;
-	String qrText = U"No text.";
-
-	//ウィンドウサイズのリサイズ
-	Window::Resize(1280, 720);
-
-	//カメラの起動
-	webcam.setResolution(1280, 720);
-	webcam.start();
-
-	while (System::Update())
-	{
-		//描画されていたものを全て消去
-		ClearPrint();
-
-		if (webcam.hasNewFrame())
-		{
-			webcam.getFrame(image);
-
-			//QRコードをデコード
-			qrDecoder.decode(image, qr.content);
-			//QRコードに赤枠を付与
-			qr.content.quad.overwriteFrame(image, 6, Palette::Red);
-			
-			if (qr.content.text)
-			{
-				qrText = qr.content.text;
-				qr.SetString();
-			}
+	std::vector<std::string> subStr;
+	std::stringstream ss(str);
+	std::string item;
+	while (std::getline(ss, item, del)) {
+		if (!item.empty()) {
+			subStr.push_back(item);
 		}
-
-		//カメラが止まっていなかったらカメラの画像を描画
-		if (!qr.isCameraStopped) {
-			texture.fill(image);
-			texture.draw();
-
-			//Enterを押すとカメラを停止
-			if (KeyEnter.down()) {
-				webcam.stop();
-				qr.isCameraStopped = true;
-			}
-		}
-
-		//読み取ったテキストを表示
-		Print << qrText;
 	}
+
+	return subStr;
+}
+
+void QRInfo::SetString() {
+	qrText = content.text.narrow();
+}
+
+Field QRInfo::GetQRContent()
+{
+	Field field;
+	Position firstPlace[2];
+	size_t h, w;
+	std::vector<std::string> splitText;
+	std::vector<std::vector<int>> splitValue;
+
+	//読み取ったテキストを行・列を維持して分割→変換
+	for (const auto tempStr : Split(qrText, ':')) {
+		splitText.push_back(tempStr);
+	}
+
+	for (int i = 0; i < splitText.size(); i++) {
+		std::vector<int> temp;
+		std::vector<std::string> splitStr = Split(splitText[i], ' ');
+
+		for (int j = 0; j < splitStr.size(); j++) {
+			temp.push_back(atoi(splitStr[j].c_str()));
+		}
+
+		splitValue.push_back(temp);
+	}
+
+	//変換した値のうち行,列,プレイヤーのいる場所を別変数に代入
+	h = splitValue[0][0];
+	w = splitValue[0][1];
+	firstPlace[0] = { splitValue[h + 1][0] - 1 ,splitValue[h + 1][1] - 1 };
+	firstPlace[1] = { splitValue[h + 2][0] - 1 ,splitValue[h + 2][1] - 1 };
+
+	//Fieldへ代入
+	for (unsigned int i = 0; i < h; i++) {
+		std::vector<Cell> cellArray;
+
+		for (unsigned int j = 0; j < w; j++) {
+			Cell tempCell;
+			tempCell.point = splitValue[i + 1][j];
+			tempCell.status = tempCell.none;
+
+			cellArray.push_back(tempCell);
+		}
+
+		field.cells.push_back(cellArray);
+	}
+
+	//プレイヤーのいる場所を保存
+	field.team1[0] = { firstPlace[0].x + 1,firstPlace[0].y + 1 };
+	field.team2[1] = { firstPlace[0].x + 1,firstPlace[1].y + 1 };
+	field.team2[0] = { firstPlace[1].x + 1,firstPlace[0].y + 1 };
+	field.team1[1] = { firstPlace[1].x + 1,firstPlace[1].y + 1 };
+
+	//プレイヤーのいる場所のステータスを変更
+	field.cells[firstPlace[0].x][firstPlace[0].y].status = Cell::team1;
+	field.cells[firstPlace[0].x][firstPlace[1].y].status = Cell::team2;
+	field.cells[firstPlace[1].x][firstPlace[0].y].status = Cell::team2;
+	field.cells[firstPlace[1].x][firstPlace[1].y].status = Cell::team1;
+
+	return field;
 }
